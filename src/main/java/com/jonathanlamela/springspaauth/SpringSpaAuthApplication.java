42fccd70f735e2f1a6422421aeebf62dafa8edc2
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-03-19 15:34:01 +0100

    first commit

---
+++ src/main/java/com/jonathanlamela/springspaauth/SpringSpaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanlamela.springspaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathanlamela/spring_spa_auth/SpringSpaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanlamela.spring_spa_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
